db3fd6a8004fa4e3eff8290d8969b98486e74269
Ajay Thanki <akthanki5@gmail.com>
2019-02-26 18:20:38 +0530

    exercise file

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 console.log("Before");
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log(commits))
-  .catch(err => console.log("Error", err.message));
+//Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log(commits))
+//   .catch(err => console.log("Error", err.message));
 
+//Async & Await Approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (error) {
+    console.log("Error : " + error.message);
+  }
+}
+displayCommits();
 console.log("After");
+
 function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -19,7 +33,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Error Message"));
     }, 2000);
   });
 }
